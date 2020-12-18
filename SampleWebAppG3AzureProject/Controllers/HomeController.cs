e2ee566ac6ebe2bb833be9ecbde52f541b5299a6
unknown <roshan.zameeralis@lntinfotech.com>
2020-12-18 10:03:25 +0530

    new commit

---
+++ SampleWebAppG3AzureProject/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleWebAppG3AzureProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Roshan Zameer Ali";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ SampleWebAppG3AzureProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SampleWebAppG3AzureProject.MvcApplication" Language="C#" %>
